d17f2f7a261c1f5a3b663c576ea07eb84a388717
Kiefer Bairstow <bairstow@usf.edu>
2025-11-05 19:25:27 -0500

    from gray to white

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Board from './components/Board';
 export default function HomePage() {
   return (
     <main className="mx-auto max-w-6xl p-6">
-      <h1 className="text-4xl font-extrabold mb-6 text-gray-900">Helpdesk Ticket Board</h1>
+      <h1 className="text-4xl font-extrabold mb-6 text-white-900">Helpdesk Ticket Board</h1>
       <Board /> 
     </main>
   );
